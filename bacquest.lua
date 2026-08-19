cef025ac4b35c83e9ae228cc8fd4ebc0e4803073
OneEight <maulaababil06@gmail.com>
2026-08-19 15:25:56 +0700

    Update bacquest.lua

--- bacquest.lua
+++ bacquest.lua
@@ -75,11 +75,11 @@ local ExpectedCarName = LocalPlayer.Name .. "sCar"
 
 -- CONFIGURABLE VALUES (DIOPTIMASI UNTUK PERJALANAN NATURAL)
 local Config = {
-	TweenSpeed = 65,        -- Kecepatan wajar CDID (~60-70 km/jam)
+	TweenSpeed = 220,        -- Kecepatan wajar CDID (~60-70 km/jam)
 	MinTravelDuration = 22, -- Batas minimal detik perjalanan agar bonus jarak penuh keluar
-	ActionDelay = 0.5,
-	LoopWait = 0.6,
-	RestartDelay = 3.0
+	ActionDelay = 0.2,
+	LoopWait = 0.1,
+	RestartDelay = 1
 }
 
 -- State
