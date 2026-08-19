a4f25ac3ff52023f877a75ad90846cd68c08e50f
OneEight <maulaababil06@gmail.com>
2026-08-20 03:45:29 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,4 @@
-local BASE_URL = "https://raw.githubusercontent.com/USERNAME/REPO_NAME/main/"
+local BASE_URL = "https://raw.githubusercontent.com/OneEight-Hub/careduit/main/"
 
 local function loadModule(path)
 	local content = game:HttpGet(BASE_URL .. path)
